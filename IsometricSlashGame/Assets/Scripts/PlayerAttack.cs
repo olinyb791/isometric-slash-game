3a1191a86bbf67fe2dc2f4af29a3c14e19c439d1
olinyb791 <olinyb791@edu.linkoping.se>
2024-03-19 14:05:08 +0100

    healthbar on hud and enemy

---
+++ IsometricSlashGame/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{ 
+    public void Slash()
+    {
+        Debug.Log("Du svingade ditt svärd!");
+    }
+
+}
